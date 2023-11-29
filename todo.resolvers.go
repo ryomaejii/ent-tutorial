9e52b6accc2828d81f81187f3ead7b9895907a98
ryomaejii <82492270+ryomaejii@users.noreply.github.com>
2023-11-29 18:50:10 +0900

    feat: Add relay server example

--- todo.resolvers.go
+++ todo.resolvers.go
@@ -11,7 +11,7 @@ import (
 
 // CreateTodo is the resolver for the createTodo field.
 func (r *mutationResolver) CreateTodo(ctx context.Context, input ent.CreateTodoInput) (*ent.Todo, error) {
- return r.client.Todo.Create().SetInput(input).Save(ctx)
+	return r.client.Todo.Create().SetInput(input).Save(ctx)
 }
 
 // Mutation returns MutationResolver implementation.
